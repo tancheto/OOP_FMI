bb29438968ee4e01b3b16f045c8cbe5ff4d7852c
Tanya Hristova <tanche98@gmail.com>
2018-04-01 01:14:44 +0300

    Update Student.h

--- StudentWithGrades/Student.h
+++ StudentWithGrades/Student.h
@@ -6,22 +6,25 @@ class Student
 private:
 	char name[15];
 	int fn;
-	int maxCount;
+	
 	int gradesCount;
 	Grade *grades;
 
 public:
+	int maxCount;
 	Student(char name[], int fn, int gradesCount, Grade *grades);
-	void setName(char name[]);
-	char*getName();
-	void setFN(int fn);
-	int getFN();
-	void setGradesNum(int);
-	int getGradesNum();
-	void setGrades(Grade *grades);
-	void addGrade(Grade g);
-	Grade* getGrades();
-	double GetAverage();
+	Student(const Student &s);
+	void setName(const char name[]);
+	const char *getName() const;
+	void setFN(const int &fn);
+	int getFN() const;
+	void setGradesNum(const int&);
+	int getGradesNum() const;
+	void setGrades(const Grade *grades);
+	Grade* getGrades() const;
+	void addGrade(const Grade &g);	
+	void deleteGrades();
+	double GetAverage() const;
 	void PrintGrades();
 	void Print();
 	void PrintAll();
